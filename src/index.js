7e0008c61c304568c068cb4666ef2d15bfa7091d
A86S\User <ajaymsharma86@gmail.com>
2018-10-01 15:38:55 +0530

    basic - added middleware into index js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducerA from './reducers/reducerA'
 import reducerB from './reducers/reducerB'
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 const rootReducer = combineReducers({
@@ -13,6 +13,16 @@ const rootReducer = combineReducers({
     rB: reducerB
 });
 
-const store = createStore(rootReducer);
+
+const logAction = store => {
+    return next => {
+        return action  => {
+            const result = next(action);
+            console.log("Caught in the middleware : "+JSON.stringify(result));
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logAction));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
